e51275538f298d7fcae5b2ee7d4f0a13bed56991
B-priyam <priyam3801h@gmail.com>
2026-02-07 13:22:01 +0530

    classes and object

--- Assign-1.py
+++ Assign-1.py
@@ -4,9 +4,28 @@ class Employee:
         self.name = name
         self.salary = salary
 
-    def getEmpDetails(self):
-        print("id",self.empId)
-        print("name",self.name)
-        print("salary",self.salary)
+    def setEmpId(self,empId):
+        self.empId = empId
+    def setName(self,name):
+        self.name = name
+    def setSalary(self,salary):
+        self.salary = salary
 
 
+    def getEmpId(self):
+        return self.empId
+    def getName(self):
+        return self.name
+    def getSalary(self):
+        return self.salary
+
+
+e1 = Employee(1,"emp1",10000)
+e2 = Employee()
+e2.setEmpId(2)
+e2.setName("emp2")
+e2.setSalary(20000)
+
+print(e1.getEmpId())
+print(e1.getName())
+print(e1.getSalary())
